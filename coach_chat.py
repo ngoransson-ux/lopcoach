735bead070c17812bf461ece7f9dff69f2d70894
ngoransson-ux <n.goransson@gmail.com>
2025-12-28 14:26:37 +0100

    Update coach_chat.py

--- coach_chat.py
+++ coach_chat.py
@@ -8,7 +8,7 @@ import time
 STRAVA_CLIENT_ID = "177985"
 STRAVA_CLIENT_SECRET = "2b3cf9adfae953c2716e0e10ef288d0eb26e4c9b"
 GEMINI_API_KEY = "AIzaSyAPFas1aeurXsPAqz6nI8mhObta316fvY4"
-REFRESH_TOKEN = st.secrets["REFRESH_TOKEN"]
+REFRESH_TOKEN = "3708e8d1bcce1c05cfd864bdc4afd47bf166aed7"
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
